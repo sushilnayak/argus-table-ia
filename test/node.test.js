d75031b65a970a04235f7808a59b11fe98962314
Sushil <sushil.nayak@gmail.com>
2017-04-17 05:59:48 +0530

    Added test case for Nodes

--- test/node.test.js
+++ test/node.test.js
@@ -4,8 +4,95 @@ import Graph from '../src/components/tableGraph';
 
 describe("Testing Node Class",function(){
 
-	it("First Test",function(done){
-		expect(1).to.equal(1);
-		done();
+	describe("Testing Nodes in following alignment A -> B; A -> C; A -> D",function(){
+		var a;
+
+		before(function(){
+			a=new Node('A');
+			a.addChild(new Node('B'))
+			a.addChild(new Node('C'))
+			a.addChild(new Node('D'))
+		})
+
+    	it('A should have 3 child node',function(done){
+    		expect(Object.keys(a.child_nodes).length).to.equal(3)
+    		done();
+    	})
+    	it('A should have 0 parent node',function(done){
+    		expect(Object.keys(a.parent_nodes).length).to.equal(0)
+    		done();
+    	})
 	})
+
+    // A -> B -> C
+    describe('Testing Nodes in following alignment A -> B -> C',function(){
+    	var a,b,c
+	    before(function(){
+
+		    a=new Node('A');
+		    b=new Node('B');
+		    c=new Node('C');
+
+	        a.addChild(b);
+	        b.addParent(a);
+
+	        b.addParent(a);
+	        b.addChild(c);
+
+	        c.addParent(b);
+
+	    })
+
+    	it('A should have 0 parent node',function(done){
+    		expect(Object.keys(a.parent_nodes).length).to.equal(0)
+    		done();
+    	})
+
+    	it('A should have 1 child node',function(done){
+    		expect(Object.keys(a.child_nodes).length).to.equal(1)
+    		done();
+    	})
+
+    	it('B should have 1 parent node',function(done){
+    		expect(Object.keys(b.parent_nodes).length).to.equal(1)
+    		done();
+    	})
+
+    	it('B should have 1 child node',function(done){
+    		expect(Object.keys(b.child_nodes).length).to.equal(1)
+    		done();
+    	})
+
+    	it('C should have 1 parent node',function(done){
+    		expect(Object.keys(c.parent_nodes).length).to.equal(1)
+    		done();
+    	})
+
+    	it('C should have 0 child node',function(done){
+    		expect(Object.keys(c.child_nodes).length).to.equal(0)
+    		done();
+    	})
+    })
+
+    
+
+
+    // before(function(){
+    //     console.log('Runs once before all test in this file')
+    // })
+    // after(function(){
+    //     console.log('Runs once after all test in this file')
+    // })
+
+    // beforeEach(function(){
+    //     console.log('Runs before every test in this file')
+    // })
+    // afterEach(function(){
+    //     console.log('Runs after every test in this file')
+    // })
+
+	// it("First Test",function(done){
+	// 	expect(1).to.equal(1);
+	// 	done();
+	// })
 })
